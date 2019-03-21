6664917bb6d4f5d1b4591eac6d7626fc661f1561
PasDeBras <paul.ponnau@gmail.com>
2019-03-21 15:47:38 +0100

    populated template view

--- view/frontend/template.php
+++ view/frontend/template.php
@@ -9,9 +9,27 @@
     <body>
         <div id="bloc_page">
             <header>
+                <div id="logo"><img src="" alt="Logo"></div>
+                <nav class="menu_gobal">
+                    <ul>
+                        <li><a href="">accueil</a></li>
+                        <li><a href="">Mission</a></li>
+                    </ul>
+                </nav>
+                <nav class="menu_user">
+                    <ul>
+                        <li><a href="">Se connecter / creer mon compte</a></li>
+                    </ul>
+                </nav>
             </header>
+
+            <div id="content">
+                <?= $content ?>
+            </div>
         </div>
         
+        
+
         <footer>
             <div>
                 <p>Créé par Paul Ponnau pour OpenClassrooms dans le cadre du parcours Web Dev<br>Contact & Information: contact@paulponnau.fr.</p>
