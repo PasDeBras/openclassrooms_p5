feb23aca0166342796c7242fefe9c7ac9154d121
PasDeBras <paul.ponnau@gmail.com>
2019-03-21 15:27:10 +0100

    added frontend html template

---
+++ view/frontend/template.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>BEEWATCH</title>
+        <link href="public/css/style.css" rel="stylesheet" /> 
+    </head>
+        
+    <body>
+        <div id="bloc_page">
+            <header>
+            </header>
+        </div>
+        
+        <footer>
+            <div>
+                <p>Créé par Paul Ponnau pour OpenClassrooms dans le cadre du parcours Web Dev<br>Contact & Information: contact@paulponnau.fr.</p>
+            </div>
+        </footer>
+    </body>
+</html>
