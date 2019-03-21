784bd5a7fb924dd7da1c1f124eff9260c55cda4d
PasDeBras <paul.ponnau@gmail.com>
2019-03-21 16:27:57 +0100

    added auth_New & auth_Verify action on index.php

--- index.php
+++ index.php
@@ -1,8 +1,13 @@
 <?php
 try {
     if (isset($_GET['action'])) {
-        if ($_GET['action'] == 'auth_start') {
+        /* User authentification */
+        if ($_GET['action'] == 'auth_start') { /* Access login or register page */
             require('controller/authentification/authStartController.php');
+        } elseif ($_GET['action'] == 'auth_New') { /* Access account registering page */
+            require('controller/authentification/authNewController.php');
+        } elseif ($_GET['action'] == 'auth_Verify') { /* Access account login page */
+            require('controller/authentification/authVerifyController.php');
         }
     } else {
         require('controller/homepageController.php');
