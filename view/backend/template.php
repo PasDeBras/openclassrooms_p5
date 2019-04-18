16d7bdcd7e3a9435e29775b1ab79ba3475359269
PasDeBras <paul.ponnau@gmail.com>
2019-04-18 18:47:38 +0200

    added backend template and account management view

---
+++ view/backend/template.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title><?= $title ?></title>
+        <link href="public/css/style.css" rel="stylesheet" /> 
+    </head>
+        
+    <body>
+        <div id="bloc_page">
+            <header>
+                <div id="logo"><img src="" alt="Logo"></div>
+                <nav class="menu_gobal">
+                    <ul>
+                        <li><a href="">accueil</a></li>
+                        <li><a href="">Mission</a></li>
+                    </ul>
+                </nav>
+                <nav class="menu_user">
+                    <ul>
+                        <li><a href="index.php?action=auth_start">Se connecter / creer mon compte</a></li>
+                    </ul>
+                </nav>
+            </header>
+
+            <div id="content">
+                <?= $content ?>
+            </div>
+        </div>
+        
+        
+
+        <footer>
+            <div>
+                <p>Créé par Paul Ponnau pour OpenClassrooms dans le cadre du parcours Web Dev<br>Contact & Information: contact@paulponnau.fr.</p>
+            </div>
+        </footer>
+    </body>
+</html>
