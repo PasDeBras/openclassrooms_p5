fae17c56f00328609bf4a16bf13c1d0be7b13bc2
PasDeBras <paul.ponnau@gmail.com>
2019-07-31 17:42:08 +0200

    whipped up a little text on the homepage

--- view/frontend/homepageView.php
+++ view/frontend/homepageView.php
@@ -1,6 +1,16 @@
 <?php $title = 'BEEWATCH- Homepage'; ?>
 
 <?php ob_start(); ?>
+<div id="missionStatement">
+    <div id="missionStatement_text">
+        <h1>Bienvenue sur BEEWATCH</h1>
+        <p>BEEWATCH est un site permettant aux apiculteurs de partout et d'ailleurs de geolocaliser leurs ruches et de partager leur position,</p>
+        <p>se prevenir les uns les autres en cas de sinistre, ou simplement de s'informer sur les autres apiculteurs à proximité.</p>
+    </div>
+    <div id="missionStatement_img">
+        <img src="css/media/beehive.png" alt="beehive img">
+    </div>
+</div>
 
 <?php $content = ob_get_clean(); ?>
 
