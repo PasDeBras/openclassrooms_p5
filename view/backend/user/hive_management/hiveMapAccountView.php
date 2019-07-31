8656064927a911a328d23c14081ba9a1e44771fe
PasDeBras <paul.ponnau@gmail.com>
2019-07-31 12:57:04 +0200

    moved couple of </a> down a peg, they are quiet now.

--- view/backend/user/hive_management/hiveMapAccountView.php
+++ view/backend/user/hive_management/hiveMapAccountView.php
@@ -3,12 +3,13 @@ $title = 'BEEWATCH- HiveMap account';
 $loadMap=TRUE; ?>
 
 <?php ob_start(); ?>
-<a href="index.php?action=hiveMap_Account_HiveCreator">Creer une ruche</a>
-<a href="index.php?action=hiveMap_Account_HiveEditor">Editer une ruche</a>
-<p>map</p>
 <section id="map_section">
     <div id="account_map"></div>
     <div id="map_overlay"></div>
+    <div id="map_hive_controls">
+        <a href="index.php?action=hiveMap_Account_HiveCreator">Creer une ruche</a>
+        <a href="index.php?action=hiveMap_Account_HiveEditor">Editer une ruche</a>
+    </div>
 </section>
 <?php $content = ob_get_clean(); ?>
 
