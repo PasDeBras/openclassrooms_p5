9edecf520c0aaf0a804e3929d414ce438677c45a
PasDeBras <paul.ponnau@gmail.com>
2019-08-01 19:04:30 +0200

    Memberlist CSS

--- view/backend/user/account_management/membersListView.php
+++ view/backend/user/account_management/membersListView.php
@@ -2,16 +2,27 @@
 $title = 'BEEWATCH- List of Members'; ?>
 
 <?php ob_start(); ?>
-<p>test</p>
 <!-- <p>Membres :<?= $listOfAccounts->rowCount(); ?></p> -->
-<?php 
-while ($data = $listOfAccounts->fetch()) {
-    if ($data['id'] != $_SESSION['id']) 
-    {?>
-        <p>Utilisateur - <?= $data['username'] ?> <a href="index.php?action=user_Account_Inbox&send_FriendshipRequest=<?= $data['id']?>">Envoyer une requete</a></p>
-    <?php }
-    }
-?>
+
+<div id="memberListDiv">
+    <h2>Liste des membres :</h2>
+    <div id="memberList_cardsContainer">
+        <?php 
+        while ($data = $listOfAccounts->fetch()) {
+            if ($data['id'] != $_SESSION['id']) 
+            {?>
+                <div class="memberCardDiv">
+                    <img src="css/media/naturalHiveColor.png">
+                    <p class="memberCardDiv_name"><?= $data['username'] ?></p>
+                    <a class="memberCardDiv_button" href="index.php?action=user_Account_Inbox&send_FriendshipRequest=<?= $data['id']?>">Ajouter aux amis</a>
+                </div>
+            <?php }
+            }
+        ?>
+    </div>
+    
+</div>
+
 <?php $content = ob_get_clean(); ?>
 
 <?php require('view/backend/template.php'); ?>
