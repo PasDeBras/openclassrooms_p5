5519f05b5b50809745cb4a07727ef48b6dafc2ec
PasDeBras <paul.ponnau@gmail.com>
2019-08-02 15:24:52 +0200

    account password change and delete view css

--- view/backend/user/account_management/accountDeleteView.php
+++ view/backend/user/account_management/accountDeleteView.php
@@ -1,12 +1,9 @@
 <?php $title = 'BEEWATCH - Supprimer mon compte'; ?>
 
 <?php ob_start(); ?>
-    <form action="index.php?action=Delete_Account" method="post">
-    Etes vous sur de vouloir supprimer ce compte :<br>
-    <input type="radio" name="delete" value=false checked> Non<br>
-    <input type="radio" name="delete" value=true> Oui<br>
-    <br>
-    <input type="submit" value="Submit">
+    <form id="deleteAccountForm"  action="index.php?action=Delete_Account&delete=1" method="post">
+    <p>Etes vous sur de vouloir supprimer ce compte ?</p>
+    <input id="deleteAccountForm_inputBtn" type="submit" value="Submit">
     
     </form>
 <?php $content = ob_get_clean(); ?>
--- view/backend/user/account_management/passwordChangeView.php
+++ view/backend/user/account_management/passwordChangeView.php
@@ -1,11 +1,13 @@
 <?php $title = 'BEEWATCH - Changer mon MDP'; ?>
 
 <?php ob_start(); ?>
-    <form action="index.php?action=Change_Password" method="post">
-    Nouveau mot de passe:<br>
-    <input type="password" name="password" id="password" required><br>
-    <br>
-    <input type="submit" value="Submit">
+    <form id="passwordChangeForm" action="index.php?action=Change_Password" method="post">
+    <div id="passwordChangeForm_typable">
+        <p id="passwordChangeForm_typable_left">Nouveau mot de passe:</p>
+        <input id="passwordChangeForm_typable_right" type="password" name="password" id="password" required>
+    </div>
+    
+    <input id="passwordChangeForm_inputBtn" type="submit" value="Confirmer">
     
     </form>
 <?php $content = ob_get_clean(); ?>
