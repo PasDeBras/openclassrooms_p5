fb1607f6c1adfd5e81207709373bc7d11298c0ba
PasDeBras <paul.ponnau@gmail.com>
2019-08-02 15:07:10 +0200

    account view css

--- view/backend/user/accountView.php
+++ view/backend/user/accountView.php
@@ -2,9 +2,15 @@
 $title = 'BEEWATCH- Mon compte'; ?>
 
 <?php ob_start(); ?>
-<p>Bienvenue, <?= $_SESSION['user_surname']?> <?= $_SESSION['user_firstname']?>.</p><br/>
-<a href="index.php?action=user_Account_Change_Password">Modifier mon mot de passe</a>
-<a href="index.php?action=user_Account_Deletion">Supprimer mon compte</a>
+<div id="accountViewDiv_container"></div>
+<div id="accountViewDiv">
+    <p><?= $_SESSION['user_surname']?> <?= $_SESSION['user_firstname']?></p><br/>
+    <div id="accountViewDiv_linksContainer">
+        <a href="index.php?action=user_Account_Change_Password">Modifier mon mot de passe</a>
+        <a href="index.php?action=user_Account_Deletion">Supprimer mon compte</a>
+    </div>
+</div>
+
 <?php $content = ob_get_clean(); ?>
 
 <?php require('view/backend/template.php'); ?>
